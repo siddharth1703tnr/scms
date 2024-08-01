86d1e36e6fd79002f268f5183a740e9779551427
BORISAGAR SIDDHARTH <codingsidhu@gmail.com>
2024-08-01 16:03:33 +0530

    Complaint.show.php basic page donne and basemodel modiyfy acoding to mysqli from PDO

--- pages/complaint/show.php
+++ pages/complaint/show.php
@@ -2,16 +2,18 @@
 require_once '../../config/config.php';
 require_once '../../classes/Database.php';
 require_once '../../classes/BaseModel.php';
-require_once '../../classes/Customer.php';
 require_once '../../classes/Complaint.php';
-require_once '../../classes/Technician.php';
-require_once '../../classes/Dealer.php';
-require_once '../../classes/ServiceCall.php';
 
 $database = new Database();
 $db = $database->getConnection();
 
-// You can now use your models here
+$complaint = new Complaint($db);
+$complaintData = null;
+
+$complaintData = $complaint->read(18);
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+}
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -19,28 +21,22 @@ $db = $database->getConnection();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-    <?php require_once('../../includes/link.php')  ?>
+    <title>Show Complaint</title>
+    <?php require_once('../../includes/link.php') ?>
 </head>
 
 <body class="hold-transition sidebar-mini layout-fixed">
     <div class="wrapper">
-
         <?php require_once('../../includes/preloder.php') ?>
         <?php require_once('../../includes/navbar.php') ?>
         <?php require_once('../../includes/asidde-st.php') ?>
-
         <div class="content-wrapper">
-            <div>
-                <h1>this is C:\xampp\htdocs\SCMS\pages\complaint\show.php </h1>
-            </div><!-- /.container-fluid -->
+          <h1>C:\xampp\htdocs\SCMS\pages\complaint\show.php</h1>
         </div>
-
         <?php require_once('../../includes/footer.php') ?>
         <?php require_once('../../includes/asidde-end.php') ?>
     </div>
-
-    <?php require_once('../../includes/script.php')  ?>
+    <?php require_once('../../includes/script.php') ?>
 </body>
 
 </html>
